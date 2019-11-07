abd9a8d7dab650866600f50858d9aa17d90a480a
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2019-11-07 22:43:06 +0200

    Base class

---
+++ OOP/Inheritance/Excercise/Restaurant/Product.cs
@@ -0,0 +1,26 @@
+namespace Restaurant
+{
+    public class Product
+    {
+        private string  name;
+        private decimal price;
+
+        public Product(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public virtual string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public virtual decimal Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+    }
+}
