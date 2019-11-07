48a359883d15184dc8fd3850d634bb381ebab22a
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2019-11-07 22:35:31 +0200

    ColdBeverage class

---
+++ OOP/Inheritance/Excercise/Restaurant/Beverages/ColdBeverage.cs
@@ -0,0 +1,11 @@
+namespace Restaurant
+{
+    public class ColdBeverage : Beverage
+    {
+        public ColdBeverage(string name, decimal price, double mililiters)
+            : base(name, price, mililiters)
+        {
+
+        }
+    }
+}
