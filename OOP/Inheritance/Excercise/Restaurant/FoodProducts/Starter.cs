36236a07c9751e82ce50835b1e4875ccd2bfd815
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2019-11-07 22:40:23 +0200

    Starter class

---
+++ OOP/Inheritance/Excercise/Restaurant/FoodProducts/Starter.cs
@@ -0,0 +1,11 @@
+namespace Restaurant.FoodProducts
+{
+    public class Starter : Food
+    {
+        public Starter(string name, decimal price, double grams)
+            : base(name, price, grams)
+        {
+          
+        }
+    }
+}
