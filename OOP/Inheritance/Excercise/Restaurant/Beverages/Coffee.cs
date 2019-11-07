b9a770761426646112916646f4b8b9fa6fe9b83e
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2019-11-07 22:37:03 +0200

    Coffee class

---
+++ OOP/Inheritance/Excercise/Restaurant/Beverages/Coffee.cs
@@ -0,0 +1,20 @@
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double coffeeMilliliters = 50;
+        private const decimal coffeePrice = 3.50M;
+
+        public Coffee(string name, double caffeine)
+            : base(name, 0, 0)
+        {
+            this.Caffeine = caffeine;
+        }
+
+        public override double Milliliters { get => coffeeMilliliters; }
+
+        public override decimal Price { get => coffeePrice; }
+
+        public double Caffeine { get; set; }
+    }
+}
