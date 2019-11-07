e926124b1a3c4e13544e491180dcc7999178ab69
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2019-11-07 22:37:28 +0200

    Tea class

---
+++ OOP/Inheritance/Excercise/Restaurant/Beverages/Tea.cs
@@ -0,0 +1,11 @@
+namespace Restaurant
+{
+    public class Tea : HotBeverage
+    {
+        public Tea(string name, decimal price, double mililiters)
+            : base (name, price, mililiters)
+        {
+
+        }
+    }
+}
