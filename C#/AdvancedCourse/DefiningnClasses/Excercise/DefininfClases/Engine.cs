30945385d6298d57594512775d2a0b68b1ec70ad
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2019-11-30 18:28:57 +0200

    Create Engine.cs

---
+++ C#/AdvancedCourse/DefiningnClasses/Excercise/DefininfClases/Engine.cs
@@ -0,0 +1,12 @@
+namespace DefiningClasses
+{
+    public class Engine
+    {
+        private int engineSpeed;
+        private int enginePower;
+
+        public int EngineSpeed { get => this.engineSpeed; set => this.engineSpeed = value; }
+
+        public int EnginePower { get => this.enginePower; set => this.enginePower = value; }
+    }
+}
