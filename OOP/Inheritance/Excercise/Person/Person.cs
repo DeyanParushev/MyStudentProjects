8fd54c1c687198731de363caef13d44f03c13a56
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2019-12-12 15:00:21 +0200

    Create Person.cs

---
+++ OOP/Inheritance/Excercise/Person/Person.cs
@@ -0,0 +1,47 @@
+namespace Person
+{
+    using System;
+    using System.Text;
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get => this.name; set => this.name = value; }
+
+        public virtual int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid age!");
+                }
+                else
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"Name: {this.Name}, Age: {this.Age}");
+
+            return sb.ToString();
+        }
+    }
+}
