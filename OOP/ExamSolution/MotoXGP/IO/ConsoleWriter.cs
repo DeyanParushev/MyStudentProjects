efc34f898d8cbb6ab8114a510ebe1b204935fa6f
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2019-12-17 13:44:31 +0200

    Create ConsoleWriter.cs

---
+++ OOP/ExamSolution/MotoXGP/IO/ConsoleWriter.cs
@@ -0,0 +1,18 @@
+namespace MXGP.IO
+{
+    using System;
+    using Contracts;
+
+    public class ConsoleWriter : IWriter
+    {
+        public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Write(string message)
+        {
+            Console.Write(message);
+        }
+    }
+}
