8695f99c3be11c378c90b6bc25bfc49414b45d9c
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2019-12-29 17:34:56 +0200

    Create SaltwaterFish.cs

---
+++ OOP/ExamSolution/AquaShop/Models/Fish/SaltwaterFish.cs
@@ -0,0 +1,16 @@
+namespace AquaShop.Models.Fish
+{
+    public class SaltwaterFish : Fish
+    {
+        public SaltwaterFish(string name, string species, decimal price) 
+            : base(name, species, price)
+        {
+            base.Size = 5;
+        }
+
+        public override void Eat()
+        {
+            base.Size += 2;
+        }
+    }
+}
