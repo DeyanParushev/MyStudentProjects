e8cdc73ce118d726bd4824551dc717843f81a4d7
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2020-01-01 12:57:54 +0200

    Derives from Animal.cs

---
+++ OOP/Inheritance/LAB/3.	HierarchicalInheritance/Cat.cs
@@ -0,0 +1,12 @@
+namespace Farm
+{
+    using System;
+
+    public class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("meowing...");
+        }
+    }
+}
