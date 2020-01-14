c82eb4d73b91835aff9781b842392b9c8b73721a
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2020-01-15 00:52:51 +0200

    Create Pet.cs

---
+++ OOP/InterfacesAndAbstraction/Excercise/BirthDayCelebration/Pet.cs
@@ -0,0 +1,18 @@
+namespace BirthDayCelebration
+{
+    public class Pet : IBirthable
+    {
+        private string name;
+        private string birthdate;
+
+        public Pet(string name, string birthdate)
+        {
+            this.Name = name;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name { get => this.name; private set => this.name = value; }
+
+        public string Birthdate { get => this.birthdate; private set => this.birthdate = value; }
+    }
+}
