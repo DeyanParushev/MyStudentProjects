c82df74e0e7a773dd6fc88435783e576ecbe65dd
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2020-01-21 04:07:46 +0200

    Provides overloading for the method Add()

---
+++ OOP/Polymorphism/Lab/MathOperations/MathOperations.cs
@@ -0,0 +1,20 @@
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public double Add(double firsNumber, double secondNumber, double thirdNumber)
+        {
+            return firsNumber + secondNumber + thirdNumber;
+        }
+
+        public decimal Add(decimal firsNumber, decimal secondNumber, decimal thirdNumber)
+        {
+            return firsNumber + secondNumber + thirdNumber;
+        }
+    }
+}
