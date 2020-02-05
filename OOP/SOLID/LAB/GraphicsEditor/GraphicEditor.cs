6726fd5f0e69087eb58f81eae9633f3b25a25753
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2020-02-05 16:48:39 +0200

    Provides the basic functionality of the program.

---
+++ OOP/SOLID/LAB/GraphicsEditor/GraphicEditor.cs
@@ -0,0 +1,12 @@
+namespace P02.Graphic_Editor
+{
+    using System;
+
+    public class GraphicEditor
+    {
+        public void DrawShape(IShape shape)
+        {
+            Console.WriteLine(shape.Draw());
+        }
+    }
+}
