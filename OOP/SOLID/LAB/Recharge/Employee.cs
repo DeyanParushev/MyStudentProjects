ba1c8ce460e0dcf73aa4fa1040f117871a2f9146
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2020-02-10 11:53:40 +0200

    Inherits the Worker base class and Implements the ISleeper interface.

---
+++ OOP/SOLID/LAB/Recharge/Employee.cs
@@ -0,0 +1,16 @@
+namespace P04.Recharge
+{
+    using System;
+
+    public class Employee : Worker, ISleeper
+    {
+        public Employee(string id) : base(id)
+        {
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Worker is sleeping");
+        }
+    }
+}
