0349c1cdc8af2f581a1fdb2906d7eaf205639daf
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2020-02-11 08:19:22 +0200

    Base class for the hierarchy.

---
+++ OOP/SOLID/LAB/Recharge/Worker.cs
@@ -0,0 +1,18 @@
+namespace P04.Recharge
+{
+    public abstract class Worker : IWorker
+    {
+        private string id;
+        private int workingHours;
+
+        public Worker(string id)
+        {
+            this.id = id;
+        }
+
+        public virtual void Work(int hours)
+        {
+            this.workingHours += hours;
+        }
+    }
+}
