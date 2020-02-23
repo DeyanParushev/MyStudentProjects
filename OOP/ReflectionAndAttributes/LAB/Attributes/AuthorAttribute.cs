65e2f375c4a9edfe6f3a782703c5601293f4cbc4
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2020-02-23 13:28:20 +0200

    Provides the Author attribute.

---
+++ OOP/ReflectionAndAttributes/LAB/Attributes/AuthorAttribute.cs
@@ -0,0 +1,16 @@
+namespace Stealer
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+
+    public class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
