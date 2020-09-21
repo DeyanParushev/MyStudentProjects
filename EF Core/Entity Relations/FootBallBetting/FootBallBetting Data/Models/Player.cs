58d61e6dc699ca7422b700d18c71f779c43c62d4
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2020-09-21 13:06:17 +0300

    Add files via upload

---
+++ EF Core/Entity Relations/FootBallBetting/FootBallBetting Data/Models/Player.cs
@@ -0,0 +1,35 @@
+﻿namespace P03_FootballBetting.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Player
+    {
+        [Key]
+        public int PlayerId { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int SquadNumber { get; set; }
+
+        [ForeignKey("Team")]
+        [Required]
+        public int TeamId { get; set; }
+
+        [Required]
+        public int PositionId { get; set; }
+
+        [Required]
+        public bool IsInjured { get; set; }
+
+        public Team Team { get; set; }
+
+        public Position Position { get; set; }
+
+        public ICollection<PlayerStatistic> PlayerStatistics { get; set; }
+    }
+}
