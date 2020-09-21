8bce67c3f1f88e5ed7df3f2f449129da79224d81
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2020-09-21 13:20:29 +0300

    Add files via upload

---
+++ EF Core/Entity Relations/FootBallBetting/FootBallBetting Data/Models/Town.cs
@@ -0,0 +1,23 @@
+﻿namespace P03_FootballBetting.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Town
+    {
+        [Key]
+        public int TownId { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        [ForeignKey("Country")]
+        public int CountryId { get; set; }
+
+        public ICollection<Team> Teams { get; set; }
+
+        public Country Country { get; set; }
+    }
+}
