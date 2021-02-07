fb29d571f4da6e22c705407943df94c9f02e1724
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2021-02-07 14:32:59 +0200

    Add files via upload

---
+++ NodeJs/config/express.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+
+module.exports = (app) => {
+    app.engine('hbs', handlebars({
+        extname: 'hbs'
+    }))
+    app.set('view engine', 'hbs')
+
+    app.use(express.static('static'))
+    app.use(express.urlencoded({
+        extended: true
+    }))
+}
