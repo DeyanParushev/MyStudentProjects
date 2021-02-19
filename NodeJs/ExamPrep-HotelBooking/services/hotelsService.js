a0e54e85a344adcdf150c8b622f5784bac8be578
Deyan Parushev <56102083+DeyanParushev@users.noreply.github.com>
2021-02-19 12:27:32 +0200

    Finish exam prepp.

--- NodeJs/ExamPrep-HotelBooking/services/hotelsService.js
+++ NodeJs/ExamPrep-HotelBooking/services/hotelsService.js
@@ -1,21 +1,18 @@
 const Hotel = require('../models/Hotel');
+const User = require('../models/User');
 
 async function getAll() {
     var hotels = await Hotel.find({}).lean();
-    hotels.sort(function(a, b) {
-        return a.freeRooms - b.freeRooms;
-    });
-
+    hotels.sort((a, b) => -(a.freeRooms > b.freeRooms) || +(a.freeRooms < b.freeRooms));
     return hotels;
 }
 
 async function getOne(_id) {
-    var hotel = await Hotel.findById(_id).populate('owner').lean();
+    var hotel = await Hotel.findById(_id).lean();
     return hotel;
 }
 
 async function create(hotel, ownerId) {
-    console.log(hotel);
     if (await Hotel.exists({ name: hotel.name })) {
         throw 'Hotel exists';
     }
@@ -23,12 +20,47 @@ async function create(hotel, ownerId) {
     if (hotel.freeRooms < 1 || hotel.freeRooms > 100) {
         throw 'Free rooms must be between 1 and 100';
     }
-
     var hotel = await Hotel.create({...hotel, owner: ownerId });
+    console.log(hotel);
+}
+
+async function update(hotel) {
+    if (hotel.name.length < 4) {
+        throw 'Hotel name cannot be less than 4 letters';
+    }
+
+    if (hotel.city.length < 3) {
+        throw 'Hotel city cannot be less than 3 letters';
+    }
+
+    if (hotel.freeRooms < 1 || hotel.freeRooms > 100) {
+        throw 'Hotel rooms must be between 1 and 100';
+    }
+
+    await Hotel.updateOne(hotel);
+}
+
+async function deleteOne(id) {
+    await Hotel.deleteOne({ _id: id });
+};
+
+async function addBooking(hotelId, userId) {
+    var hotel = await Hotel.findById(hotelId);
+    var user = await User.findById(userId);
+
+    hotel.freeRooms--;
+    hotel.usersBookedARoom.push(user._id);
+    user.bookedHotels.push(hotel._id);
+
+    await user.save();
+    await hotel.save();
 }
 
 module.exports = {
     getAll,
     getOne,
-    create
+    create,
+    update,
+    deleteOne,
+    addBooking,
 }
